90c92b310f9d37e75e4539d774f0f2389d692488
zzztttkkk <ztkisalreadytaken@gmail.com>
2023-10-23 10:24:20 +0800

    [🧱chore] (/): -

--- windows/pwsh/profile.ps1
+++ windows/pwsh/profile.ps1
@@ -370,7 +370,7 @@ function rmfs([String] $path){
 
 $global:godot_projects_path = ""
 
-function godotproject(){
+function gdp(){
 	$projects = Get-ChildItem -Directory $global:godot_projects_path | foreach-object {$_.Name}
 	$project = &gum choose $projects
 	Set-Location "$global:godot_projects_path/$project"
