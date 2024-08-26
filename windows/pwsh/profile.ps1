dd674616d64cfed639365409500cab5cfe5a88d0
zzztttkkk <ztkisalreadytaken@gmail.com>
2024-08-26 14:44:18 +0800

    [🧹 Chore] (/): -

--- windows/pwsh/profile.ps1
+++ windows/pwsh/profile.ps1
@@ -439,10 +439,41 @@ function netreset() {
 	netsh winsock reset
 }
 
-$global:__vscode_workspace_file_dir = ""
 
-function codews() {
-	code $global:__vscode_workspace_file_dir
+function __vscodechoose() {
+	param (
+		[String] $search,
+		[String] $root
+	)
+
+	$items = get-childitem -path $root | select-object -expandproperty name
+	if ( -not [string]::IsNullOrEmpty($search) ) {
+		$items = $items | where-object { $_ -like "*$search*"}
+	}
+	if ($items.Count -eq 0) {
+		return
+	}
+
+	if ($items.Count -eq 1) {
+		$name = $items
+	}else{
+		$name = gum choose $items
+	}
+
+	if ( [string]::IsNullOrEmpty($name) ) {
+		return
+	}
+
+	code $root/$name
+}
+
+$global:__code_projects_dir = ""
+
+function vsc() {
+	param (
+		[String] $search = ""
+	)
+	__vscodechoose $search $global:__code_projects_dir
 }
 
 $local = "$PSScriptRoot/local.ps1"
