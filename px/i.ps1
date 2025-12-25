418d8f7fa433d355c8970d99ce85c33fcdf4432e
zzztttkkk <ztkisalreadytaken@gmail.com>
2025-12-25 22:06:23 +0800

    [✨ Feat] (scripts): remove install.py, use pwsh

--- px/i.ps1
+++ px/i.ps1
@@ -1,2 +1,27 @@
-python3 ./scripts/install.py
+function script:install_pwsh {
+    $target = ""
+    if ($IsWindows) {
+        $target = "$HOME/Documents/PowerShell"
+    }
+    else {
+        $target = "$HOME/.config/powershell"
+    }
 
+    Remove-Item -Recurse -Force $target
+    if (!(Test-Path $target)) {
+        New-Item -ItemType Directory $target
+    }
+    Copy-Item -Recurse -Path ./pwsh -Destination $target
+}
+
+function script:install_ahk {
+    if(!$IsWindows){
+        return;
+    }
+    $target = "$HOME//AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"
+    Copy-Item -Path ./files/.ahk -Destination $target/.ahk
+}
+
+
+install_pwsh
+install_ahk
