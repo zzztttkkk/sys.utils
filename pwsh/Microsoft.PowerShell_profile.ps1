c70e372e5905d834131c53cfa2a5a8e03e8c3e65
zzztttkkk <ztkisalreadytaken@gmail.com>
2026-05-09 21:18:22 +0800

    [🚧 WIP] (/): -

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -209,17 +209,18 @@ if (Get-Command docker -ErrorAction SilentlyContinue) {
 	. $PSScriptRoot/docker.ps1
 }
 
-ensuremodule "readline"
-Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
 if (($Host.UI.RawUI.WindowSize.Width -gt 50) -and ($Host.UI.RawUI.WindowSize.Height -lt 5)) {
-	Set-PSReadLineOption -PredictionViewStyle InlineView
 }
 else {
+	ensuremodule "readline"
+	Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+	Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 	Set-PSReadLineOption -PredictionViewStyle ListView
+	Remove-PSReadLineKeyHandler -Key F2
+	Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 }
-Remove-PSReadLineKeyHandler -Key F2
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
 
 function script:reloadrc {
 	$rc = "$HOME/.pwshrc.ps1"
