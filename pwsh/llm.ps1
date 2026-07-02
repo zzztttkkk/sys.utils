b1fa23bb82e9ec0da4927e2213f563e1f694077d
zzztttkkk <ztkisalreadytaken@gmail.com>
2026-07-02 21:11:53 +0800

    [🚧 WIP] (pwsh/llmctx): perfer git

--- pwsh/llm.ps1
+++ pwsh/llm.ps1
@@ -3,6 +3,8 @@ function global:llmctx {
         [string] $root = ".",
         [alias("g")]
         [string] $glob = "*.*",
+        [alias("o")]
+        [switch] $output = $false,
         [alias("e")]
         [string[]] $exclude = @(),
         [alias("q")]
@@ -10,22 +12,51 @@ function global:llmctx {
         [alias("r")]
         [switch] $recursive = $false
     )
+    if ($output) {
+        $quiet = $true
+    }
+
+    $exclude += "*_string.go"
+
     $ingit = $true;
     git rev-parse --is-inside-work-tree 2>&1 | Out-Null
     if ($LASTEXITCODE -ne 0) {
         $ingit = $false;
     }
 
-    $exclude += "*_string.go"
+    $root = [IO.Path]::GetFullPath((Resolve-Path $root).Path).TrimEnd([IO.Path]::DirectorySeparatorChar)
 
-    $root = (Resolve-Path $root).Path
-    $gcargs = @{
-        Path    = $root
-        File    = $true
-        Filter  = $glob
-        Recurse = $recursive
+    $codes = @()
+    if ($ingit) {
+        $repo_root = git rev-parse --show-toplevel
+        $codes = @(git ls-files --cached --others --exclude-standard) | ForEach-Object {
+            if (-not ($_ -like $glob)) { return $null }
+
+            $fp = Join-Path $repo_root $_
+            $dir = Split-Path $fp -Parent
+            $dir = [IO.Path]::GetFullPath($dir).TrimEnd([IO.Path]::DirectorySeparatorChar)
+            if ($recursive) {
+                if (-not $dir.StartsWith($root)) {
+                    return $null
+                }
+            }
+            else {
+                if ($dir -ne $root) {
+                    return $null
+                }
+            }
+            return $fp
+        }
     }
-    $codes = @(Get-ChildItem @gcargs | Select-Object -ExpandProperty FullName)
+    else {
+        $gcargs = @{
+            Path    = $root
+            File    = $true
+            Filter  = $glob
+            Recurse = $recursive
+        }
+        $codes = @(Get-ChildItem @gcargs | Select-Object -ExpandProperty FullName)
+    }
     if ($codes.Count -eq 0) {
         Write-Host "empty codes: $glob" -ForegroundColor Yellow
         return
@@ -33,18 +64,12 @@ function global:llmctx {
 
     $count = 0
     $tmp = foreach ($code in $codes) {
+        if ([string]::IsNullOrEmpty($code)) { continue; }
+
         $exed = $exclude | Where-Object { 
             $code -like $_ 
         }
         if ($exed) { continue; }
-
-        if ($ingit) {
-            # TODO use `git ls-files --cached --others --exclude-standard` cache all git files in a hashset
-            git check-ignore -q -- $code 2>&1 | Out-Null
-            if ($LASTEXITCODE -eq 0) {
-                continue;
-            }
-        }
 
         $rc = Get-Content -Path $code -Encoding UTF8 -Raw
         $count += $rc.Length
@@ -56,7 +81,11 @@ function global:llmctx {
             Write-Host "JOIN $code; size: $($rc.Length)" -ForegroundColor Cyan
         }
     }
-    $tmp -join "`n" | Set-Clipboard
+
+    $result = $tmp -join "`n"
+    if ($output) { Write-Output $result }
+    else { Set-Clipboard -Value $result }
+
     if (!$quiet) {
         Write-Host "Total size: $count chars; Estimated tokens: $([math]::Round($count / 3.5))" -ForegroundColor Cyan
     }
